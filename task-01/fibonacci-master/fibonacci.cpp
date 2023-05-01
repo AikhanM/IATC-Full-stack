27e7d5bbbf862c3dde630e6bd38df9d0095ce5b3
AikhanM <103555863+AikhanM@users.noreply.github.com>
2023-05-01 21:06:28 +0400

    Task 01

---
+++ task-01/fibonacci-master/fibonacci.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int x,first,next,sum;
+    first=0;
+    next=1;
+    cin >>x;
+    while(next<=x){
+        cout<<sum<<" ";
+        sum=first+next;
+        first=next;
+        next=sum;
+        
+    }
+    
+    return 0;
+}
+
